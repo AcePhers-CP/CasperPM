0e095405cefc590e88a7eb91618d878eaf3eb7d4
Jaewook <parkjaewook228@gmail.com>
2024-07-11 15:59:54 +0900

    spring 기본 환경 생성

---
+++ demo/src/test/java/repsac/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package repsac.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
