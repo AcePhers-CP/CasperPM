df7bcfda210859b2157a8a638029056f86db79d5
Jaewook <parkjaewook228@gmail.com>
2024-08-18 19:21:55 +0900

    aws 확인

--- demo/src/main/java/repsac/demo/controller/FileLoadController.java
+++ demo/src/main/java/repsac/demo/controller/FileLoadController.java
@@ -1,7 +1,6 @@
 package repsac.demo.controller;
 
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -10,13 +9,14 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class FileLoadController {
 
-    private final AmazonS3Client s3;
+    private final AmazonS3 s3;  // AmazonS3 인터페이스로 변경
 
-    @Value("${cloud.aws.s3.bucket}") //lombok value 아님
+    @Value("${cloud.aws.s3.bucket}") // lombok의 @Value가 아니라 Spring의 @Value를 사용
     private String bucketName;
 
-    public String getPath(String path){
-        String url = s3.getUrl("repsac",path).toString();
+    public String getPath(String path) {
+        // bucketName을 사용하도록 수정
+        String url = s3.getUrl(bucketName, path).toString();
         System.out.println(url);
         return url;
     }
